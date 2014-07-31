f4f7c5a1b8295062b2cafb9895fc3d9c5f36d1a2
Yen <yen.r.plagata@gmail.com>
2014-07-31 14:26:37 -0500

    Created administrator entity class

---
+++ src/main/java/com/app/studio/model/Administrator.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.app.studio.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Entity class which holds the administrator data
+ *
+ * @author Yen
+ */
+@Entity
+@Table(name = "ADMINISTRATOR")
+public class Administrator {
+    
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Administrator{" + "id=" + id + '}';
+    }
+
+}
