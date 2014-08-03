fd52c54f1ed91bdfd1cac6ed03c11e1b0e990a8c
Yen <yen.r.plagata@gmail.com>
2014-08-03 14:32:18 -0500

    Create associations of Semester model class

--- src/main/java/com/app/studio/model/Section.java
+++ src/main/java/com/app/studio/model/Section.java
@@ -17,7 +17,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "SECTION")
 public class Section {
-    
+
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,53 +27,64 @@ public class Section {
     private String schedule;
     @ManyToOne(fetch = FetchType.EAGER)
     private Faculty faculty;
-    
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Semester semester;
+
+    public Semester getSemester() {
+        return semester;
+    }
+
+    public void setSemester(Semester semester) {
+        this.semester = semester;
+        this.semester.addSection(this);
+    }
+
     public Faculty getFaculty() {
         return faculty;
     }
-    
+
     public void setFaculty(Faculty faculty) {
         this.faculty = faculty;
         this.faculty.addsection(this);
     }
-    
+
     public int getId() {
         return id;
     }
-    
+
     public void setId(int id) {
         this.id = id;
     }
-    
+
     public int getMaxStudents() {
         return maxStudents;
     }
-    
+
     public void setMaxStudents(int maxStudents) {
         this.maxStudents = maxStudents;
     }
-    
+
     public String getLocation() {
         return location;
     }
-    
+
     public void setLocation(String location) {
         this.location = location;
     }
-    
+
     public String getSchedule() {
         return schedule;
     }
-    
+
     public void setSchedule(String schedule) {
         this.schedule = schedule;
     }
-    
+
     @Override
     public String toString() {
         return "Section{" + "id=" + id + ", maxStudents=" + maxStudents + ", location=" + location + ", schedule=" + schedule + '}';
     }
-    
+
     @Override
     public int hashCode() {
         int hash = 7;
@@ -82,9 +93,10 @@ public class Section {
         hash = 17 * hash + (this.location != null ? this.location.hashCode() : 0);
         hash = 17 * hash + (this.schedule != null ? this.schedule.hashCode() : 0);
         hash = 17 * hash + (this.faculty != null ? this.faculty.hashCode() : 0);
+        hash = 17 * hash + (this.semester != null ? this.semester.hashCode() : 0);
         return hash;
     }
-    
+
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
@@ -107,6 +119,9 @@ public class Section {
             return false;
         }
         if (this.faculty != other.faculty && (this.faculty == null || !this.faculty.equals(other.faculty))) {
+            return false;
+        }
+        if (this.semester != other.semester && (this.semester == null || !this.semester.equals(other.semester))) {
             return false;
         }
         return true;
--- src/main/java/com/app/studio/model/Semester.java
+++ src/main/java/com/app/studio/model/Semester.java
@@ -1,13 +1,19 @@
 package com.app.studio.model;
 
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
+ * Entity class which holds the semester data
  *
  * @author jCalles
  */
@@ -15,7 +21,6 @@ import javax.persistence.Table;
 @Table(name = "SEMESTER")
 public class Semester {
 
- 
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,8 +28,22 @@ public class Semester {
     private String startdate;
     private String enddate;
     private String signUpDate;
+    @OneToMany(cascade = CascadeType.REMOVE, fetch = FetchType.LAZY, mappedBy = "semester")
+    private Set<Section> setOfSections;
 
-      public int getId() {
+    public Semester() {
+        this.setOfSections = new HashSet<Section>();
+    }
+
+    public void addSection(Section section) {
+        this.setOfSections.add(section);
+    }
+
+    public Set<Section> getSetOfSections() {
+        return setOfSections;
+    }
+
+    public int getId() {
         return id;
     }
 
@@ -60,7 +79,39 @@ public class Semester {
     public String toString() {
         return "Semester{" + "id=" + id + ", startdate=" + startdate + ", enddate=" + enddate + ", signUpDate=" + signUpDate + '}';
     }
-    
-    
-    
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + this.id;
+        hash = 31 * hash + (this.startdate != null ? this.startdate.hashCode() : 0);
+        hash = 31 * hash + (this.enddate != null ? this.enddate.hashCode() : 0);
+        hash = 31 * hash + (this.signUpDate != null ? this.signUpDate.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Semester other = (Semester) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if ((this.startdate == null) ? (other.startdate != null) : !this.startdate.equals(other.startdate)) {
+            return false;
+        }
+        if ((this.enddate == null) ? (other.enddate != null) : !this.enddate.equals(other.enddate)) {
+            return false;
+        }
+        if ((this.signUpDate == null) ? (other.signUpDate != null) : !this.signUpDate.equals(other.signUpDate)) {
+            return false;
+        }
+        return true;
+    }
+
 }
