c027cccfe66897553ac47de37a7ea2ae728858e7
Yen <yen.r.plagata@gmail.com>
2014-08-10 23:14:11 -0500

    Enhanced Add Waiver Request class listing

--- src/main/java/com/app/studio/controller/WaiverRequestController.java
+++ src/main/java/com/app/studio/controller/WaiverRequestController.java
@@ -7,11 +7,13 @@ import com.app.studio.model.YogaClass;
 import com.app.studio.service.CustomerService;
 import com.app.studio.service.WaiverRequestService;
 import com.app.studio.service.YogaClassService;
+import java.security.Principal;
+import java.util.List;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -38,37 +40,42 @@ public class WaiverRequestController {
     public void setWaiverRequestService(WaiverRequestService waiverRequestService) {
         this.waiverRequestService = waiverRequestService;
     }
-    
-    
+
     @Autowired(required = true)
     @Qualifier(value = "customerService")
     public void setCustomerService(CustomerService customerService) {
         this.customerService = customerService;
     }
-    
+
 
     /* ************************* Waiver Request ************************* */
     @RequestMapping(value = "/add-waiver-request", method = RequestMethod.GET)
-    public String listYogaClasses(Model model) {
-        model.addAttribute("listYogaClasses", this.yogaClassService.listOfYogaClasses());
+    public String listYogaClasses(Principal user, Model model) {
+        getYogaClasses(user.getName(), model);
         return "add-waiver-request";
     }
 
-   
-
-    @RequestMapping(value = "/add-waiver-request/{id}/{name}", method = RequestMethod.GET)
-    public String addWaiverRequest(@PathVariable("id") int yogaClassID,
-           @PathVariable("name") String customerName, Model model) throws RequiredDataNotPresent {
-        
-        Customer customer = customerService.getCustomerByUsername(customerName);
+    @RequestMapping(value = "/add-waiver-request/{id}", method = RequestMethod.GET)
+    public String addWaiverRequest(@PathVariable("id") int yogaClassID, Principal user, Model model) throws RequiredDataNotPresent {
+        Customer customer = customerService.getCustomerByUsername(user.getName());
         YogaClass yogaClass = yogaClassService.getYogaClassByID(yogaClassID);
-        
+
         this.waiverRequestService.createNewWaiverRequest(yogaClass, customer);
         model.addAttribute("msg", "Waiver Request was created and sent successfully");
 
-        model.addAttribute("listYogaClasses", this.yogaClassService.listOfYogaClasses());
+        getYogaClasses(user.getName(), model);
         return "add-waiver-request";
-
     }
 
+    public void getYogaClasses(String username, Model model) {
+        // Get existing waivers
+        Set<WaiverRequest> waiverRequests = this.customerService.getCustomerByUsername(username).getSetOfWaiverRequests();
+        // Get all yoga classes
+        List<YogaClass> classes = this.yogaClassService.listOfYogaClasses();
+        for (WaiverRequest request : waiverRequests) {
+            classes.remove(request.getYogaClass());
+        }
+        model.addAttribute("listWaivers", waiverRequests);
+        model.addAttribute("listYogaClasses", classes);
+    }
 }
--- src/main/webapp/WEB-INF/views/add-waiver-request.jsp
+++ src/main/webapp/WEB-INF/views/add-waiver-request.jsp
@@ -3,7 +3,6 @@
     Created on : Aug 7, 2014, 10:10:12 AM
     Author     : jCalles
 --%>
-
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ taglib uri="http://www.springframework.org/tags" prefix="spring"%>
 <%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
@@ -16,7 +15,7 @@
         <link rel="stylesheet" type="text/css" href=${cssUrl}>
 
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Request to Waive a Prerequisite </title>
+        <title>Yoga Studio</title>
     </head>
     <body>
         <div id="container">
@@ -24,38 +23,44 @@
             <div id="content-container">
                 <%@include file="template/sidebar.jsp"%>
                 <div id="content">
-                    <h1>
-                        Please select the Yoga Class that you want to take
-                    </h1>
-
-
+                    <h2>
+                        Request to Waive a Prerequisite
+                    </h2>
                     <c:if test="${not empty error}">
                         <div class="error">${error}</div>
                     </c:if>
                     <c:if test="${not empty msg}">
                         <div class="msg">${msg}</div>
                     </c:if>
+                    <p>Please select the Yoga Class that you want to take</p>
                     <table class="tg">
                         <tr>
                             <th>Yoga Class ID</th>
                             <th>Name</th>
                             <th>Price</th>
-                            <th>Submit</th>
-
+                            <th>Waiver</th>
                         </tr>
+                        <c:if test="${!empty listWaivers}">
+                            <c:forEach items="${listWaivers}" var="waiver">
+                                <tr>
+                                    <td>${waiver.yogaClass.id}</td>
+                                    <td>${waiver.yogaClass.name}</td>
+                                    <td>${waiver.yogaClass.price}</td>
+                                    <td>${waiver.status}</td>
+                                </tr>
+                            </c:forEach>
+                        </c:if>
                         <c:if test="${!empty listYogaClasses}">
                             <c:forEach items="${listYogaClasses}" var="yogaClass">
                                 <tr>
                                     <td>${yogaClass.id}</td>
                                     <td>${yogaClass.name}</td>
                                     <td>${yogaClass.price}</td>
-                                    <td><a href="<c:url value='/add-waiver-request/${yogaClass.id}/${pageContext.request.userPrincipal.name}' />">Submit</a></td>
-
+                                    <td><a href="<c:url value='/add-waiver-request/${yogaClass.id}' />">Submit</a></td>
                                 </tr>
                             </c:forEach>
                         </c:if>
                     </table>
-
                 </div>
                 <!--<%@include file="template/right-side.jsp"%>-->
                 <%@include file="template/footer.jsp"%>
